9fcd10ae2daeae6415c0194adb642892c63bf898
thomaslebonnec <thomaslebonnec@icloud.com>
2025-07-04 00:47:02 +0200

    Initial Commit

---
+++ SemaphoreStudentSwift/SemaphoreStudentSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SemaphoreStudentSwiftApp.swift
+//  SemaphoreStudentSwift
+//
+//  Created by Thomas Le Bonnec on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SemaphoreStudentSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SemaphoreStudentSwiftTests/SemaphoreStudentSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  SemaphoreStudentSwiftTests.swift
+//  SemaphoreStudentSwiftTests
+//
+//  Created by Thomas Le Bonnec on 04/07/2025.
+//
+
+import Testing
+
+struct SemaphoreStudentSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
